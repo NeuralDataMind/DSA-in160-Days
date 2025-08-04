54e5a590d5b5810c14883a17593f1f0cba6a505c
B. Mallikarjun Reddy <sunnygalaxym12@gmail.com>
2025-08-04 20:39:36 +0530

    Add files via upload

---
+++ Arrays/Day_01(second_largest).py
@@ -0,0 +1,35 @@
+def print2Largest(arr: list) -> int:
+    n: int = len(arr)
+    first:int = float("-inf")
+    second: int = float("-inf") 
+
+    for num in arr:
+        if num > first:
+            second = first
+            first = num
+        elif second < num and first != num:
+            second = num
+    
+    if second != float("-inf"):
+        return second
+    else:
+        return -1
+    
+# Example 
+## Case 1
+arr: list = [1, 2, 3 ,5, 4, 4, 7 ,6, 8, 8] 
+result:int = print2Largest(arr)
+
+if result == -1:
+    print("No second value")
+else:
+    print(f"Second largest value: {result}")
+
+## Case 2
+arr: list = [1] 
+result:int = print2Largest(arr)
+
+if result == -1:
+    print("No second value")
+else:
+    print(f"Second largest value: {result}")
