d3d6434924b3c393b8b7fbd11848ea12b2a54c3d
B. Mallikarjun Reddy <sunnygalaxym12@gmail.com>
2025-08-06 19:51:44 +0530

    Update Day_03(Reverse_an_Array).py

--- Arrays/Day_03(Reverse_an_Array).py
+++ Arrays/Day_03(Reverse_an_Array).py
@@ -7,8 +7,13 @@ def Reverse_Array(arr):
         left += 1
         right -= 1
 
+    # Time Complexity = O(n)
+    # Space Complexity = O(1)
 def default_reverse_function(arr):
     arr.reverse()
+
+    # Time Complexity = O(n)
+    # Space Complexity = O(1)
 
 arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
@@ -17,3 +22,4 @@ print(arr) # Output = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
 
 default_reverse_function(arr)
 print(arr) # Output = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
