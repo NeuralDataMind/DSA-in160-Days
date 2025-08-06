3ad4b2c0998fae6a374449c16e5b5ad8a8702635
B. Mallikarjun Reddy <sunnygalaxym12@gmail.com>
2025-08-06 19:45:58 +0530

    Add files via upload

---
+++ Arrays/Day_03(Reverse an Array).py
@@ -0,0 +1,19 @@
+def Reverse_Array(arr):
+    left = 0
+    right = len(arr) - 1
+
+    while left < right:
+        arr[left], arr[right] = arr[right], arr[left]
+        left += 1
+        right -= 1
+
+def default_reverse_function(arr):
+    arr.reverse()
+
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+Reverse_Array(arr)
+print(arr) # Output = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1] 
+
+default_reverse_function(arr)
+print(arr) # Output = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
