d8a1aa43d1a162efd185fb73acb1b4985cc2f437
B. Mallikarjun Reddy <sunnygalaxym12@gmail.com>
2025-08-07 20:37:28 +0530

    Add files via upload

---
+++ Arrays/Day_03(Rotate_an_Array).py
@@ -0,0 +1,23 @@
+class Solution:
+    def reverse(self, arr, start, end):
+        while start < end:
+            arr[start], arr[end] = arr[end], arr[start]
+            start += 1
+            end -= 1
+            
+    def rotateArr(self, arr, d):
+        n = len(arr)
+        # Calculate effective rotations
+        d = d % n
+        
+        if d < 0:
+            d += n
+            
+        # Step 1: Reverse the first 'd' elements
+        self.reverse(arr, 0, d - 1)
+        
+        # Step 2: Reverse the remaining elements
+        self.reverse(arr, d, n - 1)
+        
+        # Step 3: Reverse the entire array
+        self.reverse(arr, 0, n - 1)
